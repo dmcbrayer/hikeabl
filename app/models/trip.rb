ffe62d9b2fdef76d36848625176686a32dae6674
Daniel McBrayer <dmcbrayer@gmail.com>
2014-05-18 09:48:11 -0400

    Deleted unnecessary comments from trib.rb

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,14 +8,6 @@ class Trip < ActiveRecord::Base
 	validates :name, presence: true
 	validates_date :ends_on, :on_or_after => :starts_on, :on_or_after_message => 'must be on or after starts on date'
 
-	#private
-
-	#def date_validate
-	#	if ends_on < starts_on
-	#		errors.add :base, "Your trip can't end before it starts"
-	#	end
-
-	#end
 
 end
 
