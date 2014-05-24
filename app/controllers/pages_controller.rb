5ff6add6e1303ecd9a189f6739a8c584fc1b5718
Daniel McBrayer <dmcbrayer@gmail.com>
2014-05-23 22:16:32 -0400

    Got the activity feed working on the homepage.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
+  	followees_ids = current_user.followees(User)
+  	@activities = PublicActivity::Activity.where(owner_id: followees_ids, owner_type: "User").order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
   end
 
   def about
