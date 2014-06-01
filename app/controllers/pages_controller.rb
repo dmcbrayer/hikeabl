af18cdd1390588f73659d875653c05effc53f66e
Daniel McBrayer <dmcbrayer@gmail.com>
2014-06-01 15:43:55 -0400

    added confirmable with devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
-  	followees_ids = current_user.followees(User)
+  	if user_signed_in?
+  		followees_ids = current_user.followees(User)
+  	end
   	@activities = PublicActivity::Activity.where(owner_id: followees_ids, owner_type: "User").order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
   end
 
